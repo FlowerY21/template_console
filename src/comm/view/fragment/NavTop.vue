93ba764f812304f83a54789ffbeb8039c7357ea2
zhaoyue <work_zyue@163.com>
2019-10-31 09:44:29 +0800

    完善退出功能

--- src/comm/view/fragment/NavTop.vue
+++ src/comm/view/fragment/NavTop.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="navTop flex-row vertical-center flow-justify">
     <span class="icon" :class="isCollapse ? 'el-icon-s-fold' : 'el-icon-s-unfold'" @click="handleCollapse"></span>
-    <span class="icon el-icon-switch-button"></span>
+    <span class="icon el-icon-switch-button" @click="handleExit"></span>
   </div>
 </template>
 
 <script>
+  import StorageService from 'comm/store/interfaces'
+  import Account from 'comm/service/model/Account'
+  import {mapActions, mapGetters} from 'vuex'
   export default {
     name: "navtop",
     data(){
@@ -18,6 +21,30 @@
         this.isCollapse = !this.isCollapse;
         this.$emit('handleCollapse', this.isCollapse);
       },
+      handleExit(){
+        this.$confirm('确认退出该系统？', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          showClose: false
+        }).then(() => {
+          this.$router.replace({
+            name: 'Login'
+          });
+          this.setUser(null);
+          this.setToken(null);
+          this.saveAccount(new Account({
+            i : this.formModel.userName,
+            p : ''
+          }));
+        }).catch(() => {
+
+        });
+      },
+      ...mapActions({
+        setUser: StorageService.User.setInfo,
+        setToken: StorageService.User.setToken,
+        saveAccount: StorageService.User.saveAccount
+      })
     }
   }
 </script>
@@ -27,5 +54,6 @@
     font-size: 40px;
     line-height: 80px;
     color: #333333;
+    cursor: pointer;
   }
 </style>
